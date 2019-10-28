1e9940f3afe31bde4548fc9d95b9fa63f06eb9eb
Aminov <alami@tshtt.uz>
2019-10-29 04:45:28 +0500

    2-5 2-diractional data flow

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,17 @@
 	let age=30;
 	let name='Max';
 	$: uppercaseName = name.toUpperCase();
-	//$: console.log(name);
+	$: console.log(name);
 	$: if (name === 'Maximilian' ) {//|| name === 'MAXIMILIAN'
 		console.log('It is run!');
 		age = 31;
 	}
 	function incrementAge () { age += 1;}
 	function changeName () { name = 'Maximilian';}
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
 </script>
 
 <style>
@@ -19,4 +23,5 @@
 
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
-<button on:click="{changeName}">Change Name</button>
+<!--<button on:click="{changeName}">Change Name</button>-->
+<input type="text" value="{name}" on:input="{nameInput}">
