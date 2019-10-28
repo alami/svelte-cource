282910fc8a1b10fa958806829888903b00e1931b
Aminov <alami@tshtt.uz>
2019-10-29 04:29:38 +0500

    2-4-reactive function $:

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script>
-	let age=30;function incrementAge () { age += 1;}
-	let name='Max'; function changeName () { name = 'MAXIMILIAN';}
+	let age=30;
+	let name='Max';
 	$: uppercaseName = name.toUpperCase();
+	//$: console.log(name);
+	$: if (name === 'Maximilian' ) {//|| name === 'MAXIMILIAN'
+		console.log('It is run!');
+		age = 31;
+	}
+	function incrementAge () { age += 1;}
+	function changeName () { name = 'Maximilian';}
 </script>
 
 <style>
@@ -10,6 +17,6 @@
 	}
 </style>
 
-<h1>Hello {uppercaseName}, my age is {age+1}!</h1>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
 <button on:click="{changeName}">Change Name</button>
