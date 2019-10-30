701b2a99ecb57514cc6b85493b8eb6db5963c2ca
alami <alami@km.ru>
2019-10-30 12:05:04 +0500

    6-6-see logs of our custom events

--- src/App-6-5.svelte
+++ src/App-6-5.svelte
@@ -1,9 +1,14 @@
 <script>
      import Product from "./Product-6-5.svelte";
-
+     function addToCart(event) {
+         console.log(event);
+     }
+     function deleteProduct(event) {
+         console.log(event);
+     }
 </script>
 <h1>App-6-5</h1>
 <Product
-        on:add-to-cart="{()=>alert('Add to cart!')}"
-        on:delete="{()=>alert('Delete!')}"
+        on:add-to-cart="{addToCart}"
+        on:delete="{deleteProduct}"
         productTitle="A Book" />
