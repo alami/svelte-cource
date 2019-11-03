00b351980a4fa3105f8079485b22175b81f21809
alami <alami@km.ru>
2019-11-03 18:48:41 +0500

    8-4 automatic-number-conversion

--- src/App-8.svelte
+++ src/App-8.svelte
@@ -5,8 +5,17 @@
     let selectedOption =1;
     $: console.log(val);
     function setValue(event) { val = event.target.value; }
+//<!-- ch-8-4-->
+    let price =0;
+    $: console.log(price);
+
 </script>
 <!--<input type="text" value={val}   on:input={setValue}>-->
 <!--<input type="text" value={val} bind:value={val}>-->
 <CustomInput type="text" bind:val={val} />
 <Toggle bind:chosenOption={selectedOption}/>
+
+<hr><!-- ch-8-4-->
+<input type="number"
+       bind:value={price} />
+<!--       on:input={event => console.log('1'+event.target.value)}>-->
