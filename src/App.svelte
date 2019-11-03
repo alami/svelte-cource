7c1b29ed5b561dde6636349b564117286f996674
alami <alami@km.ru>
2019-11-03 13:26:36 +0500

    7-5-creating-an-edit-meetup-component

--- src/App.svelte
+++ src/App.svelte
@@ -2,40 +2,36 @@
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
-  import Button from './UI/Button.svelte';
-
-  let title = "";
-  let subtitle = "";
-  let address = "";
-  let email = "";
-  let description = "";
-  let imageUrl = "";
+  import Button from "./UI/Button.svelte";
+  import EditMeetup from "./Meetups/EditMeetup.svelte";
 
   let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn to code in 2 hours",
-      description:
-        "In this meetup, we will have some experts that teach you how to code!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "Swim Together",
-      subtitle: "Let's go for some swimming",
-      description: "We will simply swim some rounds!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "swim@test.com",
-        isFavorite: true
-    }
+      {
+          id: "m1",
+          title: "Coding Bootcamp",
+          subtitle: "Learn to code in 2 hours",
+          description:
+                  "In this meetup, we will have some experts that teach you how to code!",
+          imageUrl:
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
+          address: "27th Nerd Road, 32523 New York",
+          contactEmail: "code@test.com",
+          isFavorite: false
+      },
+      {
+          id: "m2",
+          title: "Swim Together",
+          subtitle: "Let's go for some swimming",
+          description: "We will simply swim some rounds!",
+          imageUrl:
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+          address: "27th Nerd Road, 32523 New York",
+          contactEmail: "swim@test.com",
+          isFavorite: false
+      }
   ];
+
+  let editMode;
   function addMeetup() {
       const newMeetup = {
           id: Math.random().toString(),
@@ -73,41 +69,9 @@
 </style>
  <Header />
 <main>
-    <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-            id="title"
-            label="Title"
-            value={title}
-            on:input={event => (title = event.target.value)} />
-    <TextInput
-            id="subtitle"
-            label="Subtitle"
-            value={subtitle}
-            on:input={event => (subtitle = event.target.value)} />
-    <TextInput
-            id="address"
-            label="Address"
-            value={address}
-            on:input={event => (address = event.target.value)} />
-    <TextInput
-            id="imageUrl"
-            label="Image URL"
-            type="text"
-            value={imageUrl}
-            on:input={event => (imageUrl = event.target.value)} />
-    <TextInput
-            id="email"
-            label="E-Mail"
-            type="email"
-            value={email}
-            on:input={event => (email = event.target.value)} />
-    <TextInput
-            id="description"
-            label="Description"
-            controlType="textarea"
-            value={description}
-            on:input={event => (description = event.target.value)} />
-        <Button type="submit" caption="Save" />
-    </form>
+    <Button caption="New Meetup" on:click="{()=> editMode = 'add'}"/>
+    {#if editMode === 'add'}
+        <EditMeetup />
+    {/if}
     <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
