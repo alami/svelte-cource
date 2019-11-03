11d70ddc923059068352c01e410c14a1f1e80c84
alami <alami@km.ru>
2019-11-03 16:58:18 +0500

    8-2-two-way-binding-refresher

---
+++ src/App-8.svelte
@@ -0,0 +1,7 @@
+<script>
+    let val='Max';
+    $: console.log(val);
+    function setValue(event) { val = event.target.value; }
+</script>
+<!--<input type="text" value={val}   on:input={setValue}>-->
+    <input type="text" value={val} bind:value={val}>
